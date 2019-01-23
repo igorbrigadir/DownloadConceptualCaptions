5483290ce149488468de9523270c0765b8385ddb
Igor Brigadir <igor.brigadir@gmail.com>
2019-01-23 14:57:40 +0000

    change multiprocess part, make sure results come back in order

--- download_data.py
+++ download_data.py
@@ -1,32 +1,36 @@
 import pandas as pd
-import requests
-import os
-import zlib
-import shutil
-from multiprocessing import  Pool
-from functools import partial
 import numpy as np
-import time
+
+import requests
+import zlib
+import os
+
+from multiprocessing import  Pool
 from tqdm import tqdm
 
-# https://stackoverflow.com/questions/26784164/pandas-multiprocessing-apply
-def parallelize(data, func, num_of_processes, **kwargs):
-    data_split = np.array_split(data, 10000)
-    print(len(data_split), "parts", len(data_split[0]), "images per part, on", num_of_processes, "processes", 2, "parts in chunk.")
-    
+def _df_split_apply(tup_arg):
+    split_ind, df_split = tup_arg
+    return (split_ind, df_split.apply(download_image, axis=1))
+
+def df_multiprocess(df, processes, parts):
+    splits = np.array_split(df, parts)
+    print(len(splits), "parts", len(splits[0]), "images per part, on", processes, "processes")
+    # Keep track of splits with an index, results are fastest first, not in order
+    pool_data = [(split_ind, df_split) for split_ind, df_split in enumerate(splits)]
+
+    pbar = tqdm(desc="Downloading", total=len(df), position=0)
+
     results = []
-    with Pool(num_of_processes) as pool:
-        for i, df_ in enumerate(pool.imap_unordered(func, data_split, 2)):
-            results.append(df_)
-            img_progress.update(len(df_))
-    data = pd.concat(results, ignore_index=True)
-    return data
+    with Pool(processes) as pool:
+        for i, result in enumerate(pool.imap_unordered(_df_split_apply, pool_data, 2)):
+            results.append(result)
+            pbar.update(len(result[1]))
+    
+    results = sorted(results, key=lambda x:x[0])
+    results = pd.concat([split[1] for split in results])
 
-def run_on_subset(func, data_subset):
-    return data_subset.apply(func, axis=1)
-
-def parallelize_on_rows(data, func, num_of_processes):
-    return parallelize(data, partial(run_on_subset, func), num_of_processes)
+    pbar.close()
+    return results
 
 def download_image(row):
     # Unique name based on url
@@ -40,7 +44,6 @@ def download_image(row):
 
     try:
         # use smaller timeout to skip errors, but can result in failed downloads
-        # stream=True and shutil uses less memory but also can cause incomplete downloads
         response = requests.get(row['url'], stream=False, timeout=5) 
     except:
         # log errors later, set error as 408 timeout
@@ -53,15 +56,12 @@ def download_image(row):
             with open(fname, 'wb') as out_file:
                 # some sites respond with gzip transport encoding
                 response.raw.decode_content = True
-                #shutil.copyfileobj(response.raw, out_file)
                 out_file.write(response.content)
         except:
-            # This is if it times out during a download
+            # This is if it times out during a download or decode
             row['status'] = 408
-            return row        
-        
+            return row
         row['file'] = fname
-
     return row
 
 def open_tsv(fname, folder):
@@ -70,16 +70,22 @@ def open_tsv(fname, folder):
     df['folder'] = folder
     df['status'] = None
     df['file'] = None
-    np.random.shuffle(df.values)
+    #Shuffling here can help when restarting, but then results are out of order.
+    #np.random.shuffle(df.values)
     print("Processing", len(df), " Images:")
     return df
 
+# number of processes in the pool can be larger than cores
+num_processes = 64
+# how many images per chunk per process
+images_per_part = 4
+
+# Validation:
 df = open_tsv("Validation_GCC-1.1.0-Validation.tsv","validation")
-img_progress = tqdm(desc="Downloading...", total=len(df), position=0)
-df = parallelize_on_rows(df, download_image, 64) # number of processes in the pool can be larger than cores
+df = df_multiprocess(df=df, processes=num_processes, parts=int(len(df)/images_per_part))
 df.to_csv("downloaded_validation_report.tsv", sep='\t', header=False, index=False)
 
+# Training:
 df = open_tsv("Train_GCC-training.tsv","training")
-img_progress = tqdm(desc="Downloading...", total=len(df), position=0)
-df = parallelize_on_rows(df, download_image, 64) # number of processes in the pool can be larger than cores
+df = df_multiprocess(df=df, processes=num_processes, parts=int(len(df)/images_per_part))
 df.to_csv("downloaded_training_report.tsv", sep='\t', header=False, index=False)
